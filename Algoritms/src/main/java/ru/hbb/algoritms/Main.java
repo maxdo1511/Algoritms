7d6b8d4333aeeb788ab0a7ab42fdac80da9f447c
maxdo1511 <80486668+maxdo1511@users.noreply.github.com>
2023-04-20 19:12:23 +0300

    tree

--- Algoritms/src/main/java/ru/hbb/algoritms/Main.java
+++ Algoritms/src/main/java/ru/hbb/algoritms/Main.java
@@ -1,19 +1,21 @@
 package ru.hbb.algoritms;
 
+import ru.hbb.algoritms.UtilClasses.BinaryTree;
 import ru.hbb.algoritms.UtilClasses.Tree;
 
 public class Main {
     public static void main(String[] args) {
-        Tree tree = new Tree();
-        tree.add(10, "10");
-        tree.add(5, "5");
-        tree.add(3, "3");
-        tree.add(7, "7");
-        tree.add(11, "11");
-        tree.add(12, "12");
-        tree.add(10, "10");
-        tree.print();
-        tree.remove(5);
-        tree.print();
+        Tree binaryTree = new BinaryTree();
+        binaryTree.add(10);
+        binaryTree.add(12);
+        binaryTree.add(9);
+        binaryTree.add(8);
+        binaryTree.add(15);
+        System.out.println(binaryTree.isEmpty());
+        binaryTree.print();
+        binaryTree.delete(9);
+        binaryTree.print();
+        System.out.println(binaryTree.contains(15));
+        System.out.println(binaryTree.getDeep());
     }
 }
---
+++ Algoritms/src/main/java/ru/hbb/algoritms/UtilClasses/BinaryTree.java
@@ -0,0 +1,130 @@
+package ru.hbb.algoritms.UtilClasses;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BinaryTree implements Tree {
+
+    Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            right = null;
+            left = null;
+        }
+    }
+
+    public BinaryTree() {
+
+    }
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value);
+        }
+
+        return current;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int getDeep() {
+        return getSizeRecursive(root);
+    }
+
+    private int getSizeRecursive(Node current) {
+        return current == null ? 0 : getSizeRecursive(current.left) + 1 + getSizeRecursive(current.right);
+    }
+
+    public boolean contains(int value) {
+        return containsNodeRecursive(root, value);
+    }
+
+    private boolean containsNodeRecursive(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+
+        if (value == current.value) {
+            return true;
+        }
+
+        return value < current.value
+                ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
+    }
+
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
+    }
+
+    private Node deleteRecursive(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            if (current.right == null) {
+                return current.left;
+            }
+            if (current.left == null) {
+                return current.right;
+            }
+            //
+            int largestValue = findLargestValue(current.left);
+            current.value = largestValue;
+            current.left = deleteRecursive(current.left, largestValue);
+            return current;
+        }
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+
+    private int findLargestValue(Node root) {
+        return root.right == null ? root.value : findLargestValue(root.right);
+    }
+
+    public void print() {
+        traverseInOrder(root);
+        System.out.println();
+    }
+
+    private void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            visit(node.value);
+            traverseInOrder(node.right);
+        }
+    }
+
+    private void visit(int value) {
+        System.out.print(" " + value);
+    }
+}
